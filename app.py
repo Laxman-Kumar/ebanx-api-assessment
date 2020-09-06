cd52399564e36afb1a55b83ed06ebee670754b32
Laxman Kumar <laxman123prajapati@gmail.com>
2020-09-06 14:14:08 -0400

    Get Balance function completed

--- app.py
+++ app.py
@@ -2,5 +2,22 @@ from flask import Flask, request
 import events
 app = Flask(__name__)
 
+ACCOUNTS_DATA = {}
+
+
+@app.route('/balance', methods=['GET'])
+def get_balance():
+    account_id = request.args.get('account_id')
+
+    try:
+        balance = str(ACCOUNTS_DATA[account_id])
+        status = 200
+    except KeyError:
+        balance = '0'
+        status = 404
+
+    return balance, status
+
+
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=8001)
